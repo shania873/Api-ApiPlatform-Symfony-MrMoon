d5636fff1b8d0fec40ca62f36c5eb7e013fd20a2
shania873 <vanaeca@hotmail.com>
2023-11-01 14:43:00 +0100

    ADD refresh token

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserController extends AbstractController
@@ -16,9 +18,10 @@ class UserController extends AbstractController
     public function index(Request $request, ManagerRegistry $doctrine, EntityManagerInterface $entityManager, TokenStorageInterface $token): Response
     {
         $tokenget = $token->getToken();
-        dd($token);
+
         $user = $doctrine->getRepository(User::class)->find($tokenget->getUser()->getId());
-        $user->setLastLogin(new \DateTime('now'));
+
+        // $user->setLastLogin(new \DateTime('now'));
 
         $entityManager->flush();
         return $this->json($tokenget);
